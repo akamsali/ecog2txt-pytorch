1b6287e362410f419ee67cfcd554d2d6ba2414f7
Akshita Kamsali <akamsali@purdue.edu>
2021-06-13 23:30:38 -0400

    Add transformer model

--- ecog2txt_pytorch/dataloaders.py
+++ ecog2txt_pytorch/dataloaders.py
@@ -1,5 +1,7 @@
+import torch
 import torch.utils.data as tdata
 from tfrecord.torch.dataset import MultiTFRecordDataset
+from torch.nn.utils.rnn import pad_sequence
 
 
 class EcogDataLoader:
@@ -7,26 +9,36 @@ class EcogDataLoader:
                  tfrecord_pattern,
                  block_config,
                  subject_id,
+                 num_ECoG_channels,
                  index_pattern=None,
                  description=None
                  ):
         self.tfrecord_pattern = tfrecord_pattern
         self.block_config = block_config
         self.subject_id = subject_id
+        self.num_ECoG_channels = num_ECoG_channels
         self.index_pattern = index_pattern
         self.description = description
 
-    def get_data_loader_for_blocks(self, partition_type='training'):
+    def transform_fn(self, record):
+        # Reshapes the ecog sequence to (len_ecog_sequence , num_channels)
+        record['ecog_sequence'] = record['ecog_sequence'].reshape(-1, self.num_ECoG_channels)
+        return record
 
-        filtered_blocks = list(map(lambda y : "EFC" + self.subject_id + "_B" + y[0],
-                               filter(lambda x : x[1]["default_dataset"] == partition_type,
-                                      self.block_config.items())))
+    def pad_collate(self, batch):
+        x = (pad_sequence([torch.tensor(item[key]) for item in batch]) for key in ['ecog_sequence','text_sequence'])
+        return x
 
-        splits = { block: 1.0/len(filtered_blocks) for block in filtered_blocks }
+    def get_data_loader_for_blocks(self, batch_size=1, partition_type='training'):
+        filtered_blocks = list(map(lambda y: "EFC" + self.subject_id + "_B" + y[0],
+                                   filter(lambda x: x[1]["default_dataset"] == partition_type,
+                                          self.block_config.items())))
 
-        print("splits", splits)
+        splits = {block: 1.0 / len(filtered_blocks) for block in filtered_blocks}
+
 
         dataset = MultiTFRecordDataset(self.tfrecord_pattern,
-                                        self.index_pattern, splits, self.description)
+                                       self.index_pattern, splits, self.description,
+                                       transform=self.transform_fn)
 
-        return tdata.DataLoader(dataset, batch_size=1)
+        return tdata.DataLoader(dataset, batch_size=batch_size, collate_fn=self.pad_collate)
---
+++ ecog2txt_pytorch/vocabulary.py
@@ -0,0 +1,9 @@
+
+class Vocabulary:
+    def __init__(self, file_path):
+        self.load_from_file(file_path)
+
+    def load_from_file(self, file_path):
+        with open(file_path, 'r') as f:
+            self.words_ind_map = {x[1].strip(): x[0] for x in enumerate(f.readlines())}
+            print(self.words_ind_map)
