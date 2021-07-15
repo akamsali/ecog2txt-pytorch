63e4d66969790f526483fc651b2fc76de42c21d6
Akshita Kamsali <akamsali@purdue.edu>
2021-07-14 21:27:01 -0400

    Add Cross Validation

--- ecog2txt_pytorch/dataloaders.py
+++ ecog2txt_pytorch/dataloaders.py
@@ -32,28 +32,37 @@ class EcogDataLoader:
         # Reshapes the ecog sequence to (len_ecog_sequence , num_channels) 
         # print('ecog_len', len(record['ecog_sequence']))
         record['ecog_sequence'] = record['ecog_sequence'].reshape(-1, self.num_ECoG_channels)
-        
+
         return record
 
     def pad_collate(self, batch):
         # print([item['text_sequence'] for item in batch])
         # print("test", ([torch.tensor(item['text_sequence']) for item in batch]))
-        return (pad_sequence([torch.tensor(item[key]) for item in batch], batch_first=True) for key in ['ecog_sequence', 'text_sequence'])
-        
+        return (pad_sequence([torch.tensor(item[key]) for item in batch], batch_first=True)
+                for key in ['ecog_sequence', 'text_sequence'])
 
     def convert_to_str(self, record):
         word_ind_map_dict = self.vocabulary.words_ind_map
         # record['text_sequence'] = list(map(lambda y: y.decode(), record['text_sequence']))
-        record['text_sequence'] = list(map(lambda y: float(word_ind_map_dict[y.decode()]) if y.decode() in word_ind_map_dict else word_ind_map_dict['<OOV>'], record['text_sequence']))
+        record['text_sequence'] = list(
+            map(lambda y: float(word_ind_map_dict[y.decode()]) if y.decode() in word_ind_map_dict else
+            word_ind_map_dict['<OOV>'], record['text_sequence']))
         # print("text_sequence as list: ",record['text_sequence'])
         return self.transform_fn(record)
 
-        
-    
-    def get_data_loader_for_blocks(self, batch_size=1, partition_type='training', mode='mem'):
-        filtered_files = list(map(lambda y: self.tfrecord_path + "/EFC" + self.subject_id + "_B" + y[0] + ".tfrecord",
-                                  filter(lambda x: x[1]["default_dataset"] == partition_type,
-                                         self.block_config.items())))
+    def get_data_loader_for_blocks(self, batch_size=1, partition_type='training', mode='mem', loo_split=None):
+
+        if loo_split == None:
+            filtered_files = list(
+                map(lambda y: self.tfrecord_path + "/EFC" + self.subject_id + "_B" + y[0] + ".tfrecord",
+                    filter(lambda x: x[1]["default_dataset"] == partition_type,
+                           self.block_config.items())))
+        else:
+            filtered_files = list(
+                map(lambda y: self.tfrecord_path + "/EFC" + self.subject_id + "_B" +
+                              list(self.block_config)[y] + ".tfrecord", loo_split))
+            print(filtered_files)
+
         if mode == 'disk':
             # print("partition_type", partition_type, "filtered_files", filtered_files)
             datasets = []
@@ -63,7 +72,6 @@ class EcogDataLoader:
                 datasets.append(dataset)
 
             concat_dataset = tdata.ChainDataset(datasets)
-            print(concat_dataset)
             return tdata.DataLoader(concat_dataset, batch_size=batch_size, collate_fn=self.pad_collate, pin_memory=True)
 
         elif mode == 'mem':
--- ecog2txt_pytorch/trainers/single_subject_trainer.py
+++ ecog2txt_pytorch/trainers/single_subject_trainer.py
@@ -2,7 +2,7 @@ from ecog2txt_pytorch.models.transformer import *
 from ecog2txt_pytorch.dataloaders import EcogDataLoader
 from ecog2txt_pytorch.vocabulary import Vocabulary
 from ecog2txt_pytorch.utils.mask import *
-from ecog2txt.data_generators import ECoGDataGenerator
+from ecog2txt_pytorch.utils.training_utils import reset_weights
 
 from utils_jgm.toolbox import wer_vector
 
@@ -10,18 +10,18 @@ import torch
 import yaml
 import json
 import time
-import wandb
-
+from sklearn.model_selection import LeaveOneOut
+# import wandb
 
 
 class SingleSubjectTrainer:
     def __init__(self, subject_id, manifest_path):
         self.setup(subject_id, manifest_path)
-        
+
         self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
         self.loss_fn = torch.nn.CrossEntropyLoss(ignore_index=self.pad_idx)
-        
+
         self.manifest['optimizer']['eps'] = float(self.manifest['optimizer']['eps'])
         self.manifest['optimizer']['betas'] = [float(i) for i in self.manifest['optimizer']['betas']]
         self.optimizer = torch.optim.Adam(self.model.parameters(), **self.manifest['optimizer'])
@@ -33,55 +33,59 @@ class SingleSubjectTrainer:
     def setup(self, subject_id, manifest_path):
         with open(manifest_path, "r") as f:
             manifest_file = yaml.load(f)
-            self.manifest = manifest_file[int(subject_id)]            
-        
+            self.manifest = manifest_file[int(subject_id)]
+
         block_config_path = (self.manifest['data'])['block_config_path']
         data_path = (self.manifest['data'])['data_path']
         vocab_path = (self.manifest['data'])['vocab_path']
 
         with open(block_config_path) as bf:
             block_config_all = json.load(bf)
-        
+
         vocabulary = Vocabulary(vocab_path)
-        
-        ecog = EcogDataLoader(data_path, block_config_all[subject_id],
-                              subject_id, self.manifest, vocabulary, description=self.manifest['data']['description'])
+
+        self.block_config = block_config_all[subject_id]
+        self.ecog = EcogDataLoader(data_path, self.block_config,
+                                   subject_id, self.manifest, vocabulary,
+                                   description=self.manifest['data']['description'])
         manifest_model = self.manifest['model']
         self.manifest_model_args = self.manifest['model_args'][manifest_model.__name__]
-        
-        self.vocab_params = {'src_vocab_size': ecog.num_ECoG_channels,
+
+        self.vocab_params = {'src_vocab_size': self.ecog.num_ECoG_channels,
                              'tgt_vocab_size': len(vocabulary.words_ind_map),
                              'emb_size': self.manifest_model_args['num_out_channels']
                              }
-        
+
         self.pad_idx = vocabulary.words_ind_map['<pad>']
 
         # no extra files present in EFC400. Change validation to extra if present 
-        self.dataloaders = {'train_dataloader':
-                                ecog.get_data_loader_for_blocks(batch_size=self.manifest['training']['batch_size'],
-                                                                partition_type='training'),
-                            'test_dataloader':
-                                ecog.get_data_loader_for_blocks(batch_size=self.manifest['training']['batch_size'],
-                                                                partition_type='validation'),
-                            'val_dataloader':
-                                ecog.get_data_loader_for_blocks(batch_size=self.manifest['training']['batch_size'],
-                                                                partition_type='validation')
-                            }
+        # self.dataloaders = {'train_dataloader':
+        #                         ecog.get_data_loader_for_blocks(batch_size=self.manifest['training']['batch_size'],
+        #                                                         partition_type='training'),
+        #                     'test_dataloader':
+        #                         ecog.get_data_loader_for_blocks(batch_size=self.manifest['training']['batch_size'],
+        #                                                         partition_type='validation'),
+        #                     'val_dataloader':
+        #                         ecog.get_data_loader_for_blocks(batch_size=self.manifest['training']['batch_size'],
+        #                                                         partition_type='validation')
+        #                     }
+
         self.model = manifest_model(**self.manifest_model_args, **self.vocab_params).to(device)
 
     def train_epoch(self):
         self.model.train()
         losses = 0
         cnt = 0
-        
+
         for idx, (src, tgt) in enumerate(self.dataloaders['train_dataloader']):
             cnt += 1
             src = src.to(self.device)
-            tgt = tgt.transpose(0,1).to(self.device)
+            tgt = tgt.transpose(0, 1).to(self.device)
 
             tgt_input = tgt[:-1, :]
 
-            src_mask, tgt_mask, src_padding_mask, tgt_padding_mask = create_mask(src, self.manifest_model_args['kernel_size'], tgt_input, self.pad_idx, self.device)
+            src_mask, tgt_mask, src_padding_mask, tgt_padding_mask = create_mask(src, self.manifest_model_args[
+                'kernel_size'], tgt_input, self.pad_idx, self.device)
 
             logits = self.model(src, tgt_input, src_mask, tgt_mask,
                                 src_padding_mask, tgt_padding_mask, src_padding_mask)
@@ -103,11 +107,12 @@ class SingleSubjectTrainer:
         for idx, (src, tgt) in enumerate(self.dataloaders['val_dataloader']):
             cnt += 1
             src = src.to(self.device)
-            tgt = tgt.transpose(0,1).to(self.device)
+            tgt = tgt.transpose(0, 1).to(self.device)
 
             tgt_input = tgt[:-1, :]
 
-            src_mask, tgt_mask, src_padding_mask, tgt_padding_mask = create_mask(src, self.manifest_model_args['kernel_size'], tgt_input,self.pad_idx, self.device)
+            src_mask, tgt_mask, src_padding_mask, tgt_padding_mask = create_mask(src, self.manifest_model_args[
+                'kernel_size'], tgt_input, self.pad_idx, self.device)
 
             logits = self.model(src, tgt_input, src_mask, tgt_mask,
                                 src_padding_mask, tgt_padding_mask, src_padding_mask)
@@ -117,16 +122,17 @@ class SingleSubjectTrainer:
 
             pred_words = preds.transpose(0, 1).cpu().detach().numpy().astype(str).tolist()
             target_words = tgt_out.transpose(0, 1).cpu().detach().numpy().astype(str).tolist()
-            
-#             print("target: ", target_words)
-#             print("pred: ", pred_words)
-#             print("matched: ", target_words[target_words==pred_words])
-#             print("matched_shape: ", (target_words[target_words==pred_words]).shape)
+
+            #             print("target: ", target_words)
+            #             print("pred: ", pred_words)
+            #             print("matched: ", target_words[target_words==pred_words])
+            #             print("matched_shape: ", (target_words[target_words==pred_words]).shape)
 
             target_words_zero_count = list(map(lambda y: y.count('0'), target_words))
-            #print('tgt_zero_count', target_words_zero_count)
-            target_zero_dropped = list(map(lambda x: x[1][:len(x[1])-x[0]], zip(target_words_zero_count, target_words)))
-            pred_zero_dropped = list(map(lambda x: x[1][:len(x[1])-x[0]], zip(target_words_zero_count, pred_words)))
+            # print('tgt_zero_count', target_words_zero_count)
+            target_zero_dropped = list(
+                map(lambda x: x[1][:len(x[1]) - x[0]], zip(target_words_zero_count, target_words)))
+            pred_zero_dropped = list(map(lambda x: x[1][:len(x[1]) - x[0]], zip(target_words_zero_count, pred_words)))
             wer_vec = wer_vector(target_zero_dropped, pred_zero_dropped)
             val_wer += (sum(wer_vec) / float(len(wer_vec)))
 
@@ -135,23 +141,38 @@ class SingleSubjectTrainer:
         return losses / cnt, val_wer / cnt
 
     def train_and_evaluate(self):
-        wandb.init(project='ecog2txt-pytorch', entity='akamsali')
+        # wandb.init(project='ecog2txt-pytorch', entity='akamsali')
 
         # Magic
-        wandb.watch(self.model, log_freq=1)
+        # wandb.watch(self.model, log_freq=1)
 
-        best_val_loss = 10000
-        output_dir = self.manifest['training']['output_dir']
-        for epoch in range(1, self.manifest['training']['num_epochs'] + 1):
-            start_time = time.time()
-            train_loss = self.train_epoch()
-            end_time = time.time()
-            val_loss, val_wer = self.evaluate()
-            # change this to val_acc
-            if val_loss < best_val_loss:
-                torch.save(self.model.state_dict(), output_dir + 'best_model.pt')
+        loo = LeaveOneOut()
+        for ind, train_split, val_split in enumerate(loo.split(self.block_config)):
+            self.dataloaders = {'train_dataloader':
+                                    self.ecog.get_data_loader_for_blocks(
+                                        batch_size=self.manifest['training']['batch_size'],
+                                        loo_split=train_split),
+                                'val_dataloader':
+                                    self.ecog.get_data_loader_for_blocks(
+                                        batch_size=self.manifest['training']['batch_size'],
+                                        loo_split=val_split)
+                                }
 
-            print(
-                (f"Epoch: {epoch}, Train loss: {train_loss:.3f}, Val loss: {val_loss:.3f}, Val WER: {val_wer:.3f}"
-                 f" Epoch time = {(end_time - start_time):.3f}s"))
-            wandb.log({"train_loss": train_loss, "val_loss": val_loss, "Val WER": val_wer})
+            reset_weights(self.model)
+
+            best_val_loss = 10000
+            output_dir = self.manifest['training']['output_dir']
+            print(f"Running {ind} of {len(self.block_config)} splits")
+            for epoch in range(1, self.manifest['training']['num_epochs'] + 1):
+                start_time = time.time()
+                train_loss = self.train_epoch()
+                end_time = time.time()
+                val_loss, val_wer = self.evaluate()
+                # change this to val_acc
+                if val_loss < best_val_loss:
+                    torch.save(self.model.state_dict(), output_dir + 'best_model.pt')
+
+                print(
+                    (f"Epoch: {epoch}, Train loss: {train_loss:.3f}, Val loss: {val_loss:.3f}, Val WER: {val_wer:.3f}"
+                     f" Epoch time = {(end_time - start_time):.3f}s"))
+            # wandb.log({"train_loss": train_loss, "val_loss": val_loss, "Val WER": val_wer})
